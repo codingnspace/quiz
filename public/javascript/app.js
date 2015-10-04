47cf5f725afcf9174e74c08b21a414bd39c3a4f1
Pearl McPhee <pearlmcphee@gmail.com>
2015-10-03 23:55:47 -0700

    added serialArray()

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,22 +3,30 @@ var ronScore = 0;
 var harryScore = 0;
 var nevilleScore = 0;
 var winner ="";
+var ansArr = $('#myForm').serializeArray();
+var dataObj = {};
+
 jQuery(document).ready(function(){
 
-  $('input:radio[name="answer"]').change(
-    function(){
-        if ($(this).is(':checked') && $(this).val() == 'hermione') {
+$('#submit').submit( function(event){
+  for(var i=0; i< ansArr.length; i +=1){
+     dataObj[ansArr[i].name] = ansArr[i].value;
+
+        if (ansArr[i].is(':checked') && ansArr[i].val() === 'hermione') {
           hermioneScore += 1;
         }
-        else if ($(this).is(':checked') && $(this).val() == 'ron') {
+        else if (ansArr[i].is(':checked') && ansArr[i].val() === 'ron') {
           ronScore += 1;
         }
-      else  if ($(this).is(':checked') && $(this).val() == 'harry') {
+      else  if (ansArr[i].is(':checked') && ansArr[i].val() === 'harry') {
           harryScore += 1;
         }
-        else  if ($(this).is(':checked') && $(this).val() == 'neville') {
+        else  if (ansArr[i].is(':checked') && ansArr[i].val() === 'neville') {
           nevilleScore += 1;
         }
+  }
+
+  showWinner();
 });
 
 });
